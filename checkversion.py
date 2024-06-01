f39278f42632f019d8764674bc0025e9df802568
Danjay <danan12453@gmail.com>
2024-06-01 19:19:18 +0700

    update Readme

---
+++ checkversion.py
@@ -0,0 +1,6 @@
+import cv2
+print("OpenCV version:", cv2.__version__)
+import matplotlib
+print("Matplotlib Version",matplotlib.__version__)
+import numpy as np
+print("Numpy Version",np.__version__)
